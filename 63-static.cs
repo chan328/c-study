703c08bdd7d4628d524ff390e5b9dd5ada035a82
chan <rkdcks020328@gmail.com>
2018-07-12 19:42:19 +0900

    07/12

---
+++ 63-static.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy.source
+{
+    class Class1
+    {
+        public Employee (string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = NextId;
+            NextId++;
+        }
+
+
+        public static int NextId;
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Salary { get; set; } = "Not Enough";
+    }
+}
---
+++ 64-static_field.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy.source
+{
+    class Class2
+    {
+        static void Main()
+        {
+            Employee.NextId = 1000000;
+            Employee employee1 = new Employee("Indigo", "Montoya");
+            Employee employee2 = new Employee("Princess", "Buttercup");
+
+            Console.WriteLine(
+                "{0} {1} ({2})",
+                employee1.FirstName,
+                employee1.LastName,
+                employee1.Id);
+            Console.WriteLine(
+                "{0} {1} ({2})",
+                employee2.FirstName,
+                employee2.LastName,
+                employee2.Id);
+
+            Console.WriteLine(
+                $"NextId = {Employee.NextId}");
+        }
+    }
+}
